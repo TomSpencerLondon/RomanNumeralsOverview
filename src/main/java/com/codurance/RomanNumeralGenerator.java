579bb140101bc0a1e024e51626475b65a7c4614f
Tom <thomas.spencer@codurance.com>
2020-08-07 16:42:12 +0100

    Reverse nums in order

--- src/main/java/com/codurance/RomanNumeralGenerator.java
+++ src/main/java/com/codurance/RomanNumeralGenerator.java
@@ -1,15 +1,28 @@
 package com.codurance;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class RomanNumeralGenerator {
 
   public String convert(int arabic) {
     StringBuilder result = new StringBuilder();
 
-    Map<Integer, String> numerals = Map.of(5, "V", 4, "IV", 1, "I");
+    SortedMap<Integer, String> numerals = new TreeMap<>();
 
-    for (Integer number : numerals.keySet()){
+    numerals.put(1, "I");
+    numerals.put(4, "IV");
+    numerals.put(5, "V");
+    List<Integer> nums = new ArrayList<>(numerals.keySet());
+    Collections.reverse(nums);
+
+    for (Integer number : nums){
       while(arabic >= number){
         result.append(numerals.get(number));
         arabic -= number;
