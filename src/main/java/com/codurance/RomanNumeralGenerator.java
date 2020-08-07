42c028e8257dfaaf58f475ea99e1e82e40db9ef8
Tom <thomas.spencer@codurance.com>
2020-08-07 08:36:48 +0100

    Simplify with repeat method

--- src/main/java/com/codurance/RomanNumeralGenerator.java
+++ src/main/java/com/codurance/RomanNumeralGenerator.java
@@ -3,9 +3,6 @@ package com.codurance;
 public class RomanNumeralGenerator {
 
   public String convert(int arabic) {
-    if (arabic == 1){
-      return "I";
-    }
-    return "II";
+    return "I".repeat(arabic);
   }
 }
--- src/test/java/com/codurance/RomanNumeralGeneratorShould.java
+++ src/test/java/com/codurance/RomanNumeralGeneratorShould.java
@@ -2,19 +2,31 @@ package com.codurance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralGeneratorShould {
 
+  private RomanNumeralGenerator romanNumeral;
+
+  @BeforeEach
+  void setUp() {
+    romanNumeral = new RomanNumeralGenerator();
+  }
+
   @Test
   void return_I_for_1() {
-    RomanNumeralGenerator romanNumeral = new RomanNumeralGenerator();
     assertEquals("I", romanNumeral.convert(1));
   }
 
   @Test
   void return_II_for_2() {
-    RomanNumeralGenerator romanNumeral = new RomanNumeralGenerator();
     assertEquals("II", romanNumeral.convert(2));
+  }
+
+  @Test
+  void return_III_for_3() {
+    assertEquals("III", romanNumeral.convert(3));
   }
 }
