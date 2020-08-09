89513009d835794eb6e80430ecfbfd74fb9af988
Tom <thomas.spencer@codurance.com>
2020-08-09 23:02:20 +0100

    Add enum

--- src/main/java/com/codurance/RomanNumeralGenerator.java
+++ src/main/java/com/codurance/RomanNumeralGenerator.java
@@ -5,14 +5,11 @@ public class RomanNumeralGenerator {
   public static String romanFor(int decimal) {
     String roman = "";
 
-    if (decimal >= 10){
-      roman += "X";
-      decimal -= 10;
-    }
-
-    if (decimal >= 5){
-      roman += "V";
-      decimal -= 5;
+    for (RomanToDecimal romanToDecimal : RomanToDecimal.values()){
+      if (decimal >= romanToDecimal.decimal){
+        roman += romanToDecimal.roman;
+        decimal -= romanToDecimal.decimal;
+      }
     }
 
     for (int i = 0; i < decimal; ++i){
@@ -20,5 +17,18 @@ public class RomanNumeralGenerator {
     }
 
     return roman;
+  }
+
+  enum RomanToDecimal {
+    TEN("X", 10),
+    FIVE("V", 5);
+
+    private final String roman;
+    private final int decimal;
+
+    RomanToDecimal(String roman, int decimal) {
+      this.roman = roman;
+      this.decimal = decimal;
+    }
   }
 }
